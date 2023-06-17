ca80931feab33510d50d1aba779b2549dc913b83
kelz9876 <kelzzzz97@gmail.com>
2023-06-17 08:15:06 -0700

    zone

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-	
 	printf("Size of a char: %lu byte(S)\n", sizeof(char));
 	printf("Size of an int: %lu byte(S)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(S)\n", sizeof(long int));
