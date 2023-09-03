1b98b5c6cb7f595b81e89b9eea89a5c56d54f4de
kelz9876 <kelzzzz97@gmail.com>
2023-09-02 22:48:57 -0700

    mmm

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -9,14 +9,16 @@
  * returns : 0
  */
 
-int _isupper(int c)
-{
-	if(c >= 65 && c <= 90)
-	{
-		return (1);
-	}
 
-	return(0);
+int main(void)
+{
+        char c;
+
+        c = 'A';
+        printf("%c: %d\n", c, _isupper(c));
+        c = 'a';
+        printf("%c: %d\n", c, _isupper(c));
+        return (0);
 }
 ~                                                                   
 ~                         
