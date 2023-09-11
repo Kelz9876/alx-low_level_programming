7e97f9538368aa47bb52195c8be98cb5d3ad3ec9
kelz9876 <kelzzzz97@gmail.com>
2023-09-10 17:22:42 -0700

    m

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,30 +1,29 @@
-#include "main.h"	
-/**
-*  _strcat - function that concatenates two strings
-*  @dest: entered value
-*  @src: entered value
-*
-*  Return: void
-*/
-char *_strcat(char *dest, char *src)
+#include "main.h"
+	/**
+	 *  _strcat - function that concatenates two strings
+	 *  @dest: entered value
+	 *  @src: entered value
+	 *
+	 *  Return: void
+	 */
+	char *_strcat(char *dest, char *src)
 {
 	int a;
-	int b
-
+	int b;
+	
 	a = 0;
 	while (dest[a] != '\0')
 	{
-		a++;
+	a++;
 	}
 	b = 0;
 	while (src[b] != '\0')
 	{
-	dest[a] = src[b]:
-		a++;
-	        b++;
-	        	}
-	 
-	        	dest[a] = '\0';
-	        	return (dest);
+	dest[a] = src[b];
+	a++;
+	b++;
 	}
 
+	dest[a] = '\0';
+	return (dest);
+}
--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,3 +1,4 @@
+ 
 #include "main.h"
 /**
  * _atoi - convert a string into an integer.
